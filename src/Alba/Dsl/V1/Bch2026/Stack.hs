282bd1ca27006f9323e4ea82ee1656f75cdbc42d
albaDsl <albadsl@proton.me>
2025-05-23 17:49:05 +0200

    Make lambdas part of StackEntry.

--- src/Alba/Dsl/V1/Bch2026/Stack.hs
+++ src/Alba/Dsl/V1/Bch2026/Stack.hs
@@ -2,6 +2,12 @@
 
 module Alba.Dsl.V1.Bch2026.Stack (TLambda, TLambdaUntyped) where
 
+import Alba.Dsl.V1.Bch2025.Stack (StackEntry)
+
 data TLambda s
 
 data TLambdaUntyped
+
+instance StackEntry (TLambda s)
+
+instance StackEntry TLambdaUntyped
