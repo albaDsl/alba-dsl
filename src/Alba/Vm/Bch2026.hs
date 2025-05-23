f69f452fc4a17f3c5d00f37d5d96fdf86f36a0fd
albaDsl <albadsl@proton.me>
2025-05-23 17:18:11 +0200

    Add Bch2026.VmParams.

--- src/Alba/Vm/Bch2026.hs
+++ src/Alba/Vm/Bch2026.hs
@@ -2,13 +2,13 @@
 
 module Alba.Vm.Bch2026
   ( module Alba.Vm.Bch2026.Vm,
-    module Alba.Vm.Bch2025.VmParams,
+    module Alba.Vm.Bch2026.VmParams,
     module Alba.Vm.Common,
     module Alba.Vm.Common.TxContext,
   )
 where
 
-import Alba.Vm.Bch2025.VmParams
+import Alba.Vm.Bch2026.VmParams
 import Alba.Vm.Bch2026.Vm
 import Alba.Vm.Common
 import Alba.Vm.Common.TxContext
