58d0957fa7997926b0e56ab93402ef1d3566e00d
albaDsl <albadsl@proton.me>
2025-06-02 20:03:12 +0200

    Add opUntil to DSL.

--- src/Alba/Dsl/V1/Bch2026/Ops.hs
+++ src/Alba/Dsl/V1/Bch2026/Ops.hs
@@ -5,8 +5,13 @@ module Alba.Dsl.V1.Bch2026.Ops where
 import Alba.Dsl.V1.Bch2026.Stack (TLambda)
 import Alba.Dsl.V1.Common.CompilerUtils (aop)
 import Alba.Dsl.V1.Common.FlippedCons (type (>))
-import Alba.Dsl.V1.Common.Stack (S (S))
+import Alba.Dsl.V1.Common.Stack (FNA, S (S), TBool)
 import Alba.Vm.Common.OpcodeL2 (OpcodeL2 (..))
+
+opUntil :: FNA s alt (s > TBool) alt -> FNA s alt s alt
+opUntil loopBody (S c) =
+  let (S c') = loopBody (S (aop c OP_BEGIN))
+   in S (aop c' OP_UNTIL)
 
 opEval :: S (s > TLambda (S s alt -> S s' alt')) alt -> S s' alt'
 opEval (S c) = S (aop c OP_EVAL)
