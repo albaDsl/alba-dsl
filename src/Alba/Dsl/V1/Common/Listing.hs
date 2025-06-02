5787205ecfeadd1c3d40a475f7b0210d22dc33b0
albaDsl <albadsl@proton.me>
2025-06-02 20:03:12 +0200

    Add code size to listing output.

--- src/Alba/Dsl/V1/Common/Listing.hs
+++ src/Alba/Dsl/V1/Common/Listing.hs
@@ -3,17 +3,27 @@
 
 module Alba.Dsl.V1.Common.Listing (list, listStr) where
 
-import Alba.Vm.Common.OpcodeL2 (CodeL2, OpcodeL2 (..))
+import Alba.Vm.Common.OpcodeL2 (CodeL2, OpcodeL2 (..), codeL2ToCodeL1)
+import Data.ByteString qualified as B
+import Data.Maybe (fromMaybe)
 import Data.Sequence (Seq ((:<|)))
 import Data.Sequence qualified as S
+import Text.Printf (printf)
 
 list :: CodeL2 -> IO ()
-list = list' ""
+list code = do
+  list' "" code
+  printf
+    "\n%d opcodes, %d bytes.\n"
+    (S.length code)
+    (B.length $ fromMaybe err (codeL2ToCodeL1 code))
   where
+    err = error "list: internal error."
+
     list' :: String -> CodeL2 -> IO ()
-    list' _indent code | S.null code = pure ()
-    list' indent code = do
-      let (op :<| code') = code
+    list' _indent code' | S.null code' = pure ()
+    list' indent code' = do
+      let (op :<| code'') = code'
       putStrLn $
         case op of
           OP_ELSE -> drop 2 indent <> show op
@@ -24,7 +34,7 @@ list = list' ""
               OP_IF -> indent <> "  "
               OP_ENDIF -> drop 2 indent
               _ -> indent
-      list' indent' code'
+      list' indent' code''
 
 listStr :: CodeL2 -> String
 listStr code | S.null code = ""
