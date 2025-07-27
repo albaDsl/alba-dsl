7da0875ba64a4eba3b8be7add0c924343ffe4f77
albaDsl <albadsl@proton.me>
2025-07-27 14:53:31 +0200

    Untyped opUntil.

---
+++ src/Alba/Dsl/V1/Bch2026/OpsUntyped.hs
@@ -0,0 +1,12 @@
+-- Copyright (c) 2025 albaDsl
+
+module Alba.Dsl.V1.Bch2026.OpsUntyped where
+
+import Alba.Dsl.V1.Common.CompilerUtils (aop)
+import Alba.Dsl.V1.Common.StackUntyped (FNU, SU (SU))
+import Alba.Vm.Common.OpcodeL2 (OpcodeL2 (..))
+
+opUntil :: FNU -> FNU
+opUntil loopBody (SU c fs) =
+  let (SU c' fs') = loopBody (SU (aop c OP_BEGIN) fs)
+   in SU (aop c' OP_UNTIL) fs'
