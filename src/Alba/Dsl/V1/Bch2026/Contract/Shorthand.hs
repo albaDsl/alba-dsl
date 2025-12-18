22b91ac7d11e30113d95b84937e9a4743e79f0a3
albaDsl <albadsl@proton.me>
2025-12-18 10:56:45 +0100

    Ops shorthand.

---
+++ src/Alba/Dsl/V1/Bch2026/Contract/Shorthand.hs
@@ -0,0 +1,38 @@
+-- Copyright (c) 2025 albaDsl
+-- Experimental shorthand.
+
+module Alba.Dsl.V1.Bch2026.Contract.Shorthand where
+
+import Alba.Dsl.V1.Bch2026
+
+toAlt :: (StackEntry x1) => FNA (s > x1) alt s (alt > x1)
+toAlt = opToAltStack
+
+fromAlt :: (StackEntry x1) => FNA s (alt > x1) (s > x1) alt
+fromAlt = opFromAltStack
+
+depth :: FN s (s > TNat)
+depth = opDepth
+
+drop :: (StackEntry x1) => FN (s > x1) s
+drop = opDrop
+
+dup :: (StackEntry x1) => FN (s > x1) (s > x1 > x1)
+dup = opDup
+
+nip :: (StackEntry x1, StackEntry x2) => FN (s > x1 > x2) (s > x2)
+nip = opNip
+
+over :: (StackEntry x1, StackEntry x2) => FN (s > x1 > x2) (s > x1 > x2 > x1)
+over = opOver
+
+rot ::
+  (StackEntry x1, StackEntry x2, StackEntry x3) =>
+  FN (s > x1 > x2 > x3) (s > x2 > x3 > x1)
+rot = opRot
+
+swap :: (StackEntry x1, StackEntry x2) => FN (s > x1 > x2) (s > x2 > x1)
+swap = opSwap
+
+tuck :: (StackEntry x1, StackEntry x2) => FN (s > x1 > x2) (s > x2 > x1 > x2)
+tuck = opTuck
