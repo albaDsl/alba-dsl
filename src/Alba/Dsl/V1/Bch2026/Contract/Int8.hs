0741f52f86d6b5255a8e3f90d02e3292012dd553
albaDsl <albadsl@proton.me>
2025-12-18 10:56:45 +0100

    Add Int8 library.

---
+++ src/Alba/Dsl/V1/Bch2026/Contract/Int8.hs
@@ -0,0 +1,57 @@
+-- Copyright (c) 2025 albaDsl
+
+module Alba.Dsl.V1.Bch2026.Contract.Int8 (TInt8, int8, toInt8) where
+
+import Alba.Dsl.V1.Bch2026
+  ( FN,
+    StackEntry,
+    TInt,
+    begin,
+    cast,
+    function,
+    int,
+    lambda1,
+    nat,
+    opBin2Num,
+    opNum2Bin,
+    opVerify,
+    opWithin,
+    (#),
+    type (>),
+  )
+import Alba.Dsl.V1.Bch2026.Contract.PackFs (PackFs (..), TPackFs, mkPackFs)
+import Alba.Dsl.V1.Bch2026.Contract.Shorthand (dup)
+import Control.Exception (assert)
+
+data TInt8
+
+instance StackEntry TInt8
+
+instance PackFs TInt8 where
+  sizeConst = 1
+  size = nat (sizeConst @TInt8)
+  pack = cast # size @TInt8 # opNum2Bin
+  unpack = opBin2Num # cast
+  record = int8PackFs
+
+int8PackFs :: FN s (s > TPackFs TInt8)
+int8PackFs =
+  function
+    ( begin
+        # size @TInt8
+        # lambda1 (pack @TInt8)
+        # lambda1 (unpack @TInt8)
+        # mkPackFs
+    )
+
+int8Max :: Integer
+int8Max = 127
+
+int8Min :: Integer
+int8Min = -int8Max
+
+int8 :: Integer -> FN s (s > TInt8)
+int8 x = assert (x >= int8Min && x <= int8Max) (int x # cast)
+
+toInt8 :: FN (s > TInt) (s > TInt8)
+toInt8 = dup # int int8Min # int int8Max # opWithin # opVerify # cast
