8bdc45b06f86c9ccd1f182628729fb0ea75dfa2e
albaDsl <albadsl@proton.me>
2025-12-18 10:56:45 +0100

    Add Int64 library.

---
+++ src/Alba/Dsl/V1/Bch2026/Contract/Int64.hs
@@ -0,0 +1,57 @@
+-- Copyright (c) 2025 albaDsl
+
+module Alba.Dsl.V1.Bch2026.Contract.Int64 (TInt64, int64, toInt64) where
+
+import Alba.Dsl.V1.Bch2026
+  ( FN,
+    StackEntry,
+    TInt,
+    begin,
+    cast,
+    function,
+    int,
+    lambda1,
+    nat,
+    opBin2Num,
+    opNum2Bin,
+    opVerify,
+    opWithin,
+    (#),
+    type (>),
+  )
+import Alba.Dsl.V1.Bch2026.Contract.PackFs (PackFs (..), TPackFs, mkPackFs)
+import Alba.Dsl.V1.Bch2026.Contract.Shorthand (dup)
+import Control.Exception (assert)
+
+data TInt64
+
+instance StackEntry TInt64
+
+instance PackFs TInt64 where
+  sizeConst = 8
+  size = nat (sizeConst @TInt64)
+  pack = cast # size @TInt64 # opNum2Bin
+  unpack = opBin2Num # cast
+  record = int64PackFs
+
+int64PackFs :: FN s (s > TPackFs TInt64)
+int64PackFs =
+  function
+    ( begin
+        # size @TInt64
+        # lambda1 (pack @TInt64)
+        # lambda1 (unpack @TInt64)
+        # mkPackFs
+    )
+
+int64Max :: Integer
+int64Max = 9223372036854775807
+
+int64Min :: Integer
+int64Min = -int64Max
+
+int64 :: Integer -> FN s (s > TInt64)
+int64 x = assert (x >= int64Min && x <= int64Max) (int x # cast)
+
+toInt64 :: FN (s > TInt) (s > TInt64)
+toInt64 = dup # int int64Min # int int64Max # opWithin # opVerify # cast
