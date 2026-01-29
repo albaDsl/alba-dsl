64555ebcba62159d992bdc42e5a9eda38858cbda
albaDsl <albadsl@proton.me>
2026-01-29 12:10:26 +0100

    Also export Bch2026.LangArgs.

--- src/Alba/Dsl/V1/Bch2026.hs
+++ src/Alba/Dsl/V1/Bch2026.hs
@@ -4,11 +4,13 @@ module Alba.Dsl.V1.Bch2026
   ( module Alba.Dsl.V1.Bch2025,
     module Alba.Dsl.V1.Bch2026.Ops,
     module Alba.Dsl.V1.Bch2026.Lang,
+    module Alba.Dsl.V1.Bch2026.LangArgs,
     module Alba.Dsl.V1.Bch2026.Stack,
   )
 where
 
 import Alba.Dsl.V1.Bch2025
 import Alba.Dsl.V1.Bch2026.Lang
+import Alba.Dsl.V1.Bch2026.LangArgs
 import Alba.Dsl.V1.Bch2026.Ops
 import Alba.Dsl.V1.Bch2026.Stack
