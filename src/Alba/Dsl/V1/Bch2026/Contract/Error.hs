e1d9b2b5897ffdcbc41d4c4e48fc222ff15178e1
albaDsl <albadsl@proton.me>
2026-04-12 14:38:19 +0200

    Error functions.

---
+++ src/Alba/Dsl/V1/Bch2026/Contract/Error.hs
@@ -0,0 +1,18 @@
+-- Copyright (c) 2026 albaDsl
+
+module Alba.Dsl.V1.Bch2026.Contract.Error where
+
+import Alba.Dsl.V1.Bch2026
+import Prelude ()
+
+error :: FnA (s > TBytes) alt s' alt'
+error = opFalse # opVerify # castStack
+
+error' :: FnA s alt s' alt'
+error' = opFalse # opVerify # castStack
+
+errCanNotHappen :: FnA (s > TBytes) alt s' alt'
+errCanNotHappen = bytes "E0" # error
+
+errPartialFunction :: FnA (s > TBytes) alt s' alt'
+errPartialFunction = bytes "E1" # error
