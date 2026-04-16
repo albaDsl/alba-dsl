3b9586378c8f56439662e0ce1d1d48187b59e480
albaDsl <albadsl@proton.me>
2026-04-16 12:03:50 +0200

    Refactor and optimize Jacobian add.

--- src/DslDemo/EllipticCurve/JacobianAdd.hs
+++ src/DslDemo/EllipticCurve/JacobianAdd.hs
@@ -6,8 +6,8 @@ import Alba.Dsl.V1.Bch2026
   ( Fn,
     TInt,
     begin,
+    cond,
     del,
-    ex1,
     fn,
     int,
     name,
@@ -31,29 +31,26 @@ import DslDemo.EllipticCurve.JacobianPoint
     makeIdentity,
     makePoint,
   )
-import Prelude ()
+import Prelude (Integer)
 
 ecDoubleJ :: Fn (s > TPointJ) (s > TPointJ)
 ecDoubleJ =
   fn
     ( begin
-        . ns #p
-        . (pick #p . isIdentity)
+        . (ns #p . pick #p . isIdentity)
         . opIf
           (roll #p)
           ( begin
               . name3 #x #y #z (roll #p . getXYZ')
-              . name #s (int 4 . pick #x . feMul . pick #y . term2)
-              . name #m (int 3 . roll #x . term2)
-              . name #x' (pick #m . feSquare . pick #s . int 2 . feMul . feSub)
-              . name
-                #y'
+              . name #s (pick #x . coeff 4 . pick #y . term2)
+              . name #m (roll #x . feSquare . coeff 3)
+              . name #x' (pick #m . feSquare . pick #s . coeff 2 . feSub)
+              . (name #y')
                 ( begin
                     . (roll #m . roll #s . pick #x' . feSub . feMul)
-                    . ex1 (int 8 . pick #y . term4)
-                    . feSub
+                    . (int 8 . pick #y . term4 . feSub)
                 )
-              . name #z' (int 2 . roll #y . roll #z . feMul . feMul)
+              . name #z' (roll #y . roll #z . feMul . coeff 2)
               . (roll #x' . roll #y' . roll #z' . makePoint)
           )
     )
@@ -63,13 +60,11 @@ ecAddJ =
   fn
     ( begin
         . ns2 #p1 #p2
-        . (pick #p1 . isIdentity)
-        . opIf
-          (roll #p2 . del #p1)
-          ( begin
-              . (pick #p2 . isIdentity)
-              . opIf (roll #p1 . del #p2) (roll #p1 . roll #p2 . doAdd)
-          )
+        . cond
+          [ (pick #p1 . isIdentity, roll #p2 . del #p1),
+            (pick #p2 . isIdentity, roll #p1 . del #p2)
+          ]
+          (roll #p1 . roll #p2 . doAdd)
     )
 
 doAdd :: Fn (s > TPointJ > TPointJ) (s > TPointJ)
@@ -80,36 +75,32 @@ doAdd =
     . name3 #x2 #y2 #z2 (roll #p2 . getXYZ')
     . name2 #u1 #u2 (roll #x1 . pick #z2 . term2 . roll #x2 . pick #z1 . term2)
     . name2 #s1 #s2 (roll #y1 . pick #z2 . term3 . roll #y2 . pick #z1 . term3)
-    . ex1 (pick #u1 . pick #u2 . equal)
+    . (pick #u1 . pick #u2 . equal)
     . opIf
       ( begin
-          . (del #z1 . del #z2 . del #u1 . del #u2)
-          . (roll #s1 . roll #s2 . equal . opNot)
+          . (del #u2 . del #u1 . del #z2 . del #z1)
+          . (roll #s2 . roll #s1 . equal . opNot)
           . opIf (del #p1 . makeIdentity) (roll #p1 . ecDoubleJ)
       )
       ( begin
           . name #h (roll #u2 . pick #u1 . feSub)
           . name #r (roll #s2 . pick #s1 . feSub)
-          . name
-            #x3
+          . (name #x3)
             ( begin
-                . ex1 (pick #r . feSquare . pick #h . feCube . feSub)
-                . ex1 (int 2 . pick #u1 . pick #h . term2 . feMul)
-                . feSub
+                . (pick #r . feSquare . pick #h . feCube . feSub)
+                . (pick #u1 . pick #h . term2 . coeff 2 . feSub)
             )
-          . name
-            #y3
+          . (name #y3)
             ( begin
-                . ( begin
-                      . (roll #r . roll #u1 . pick #h . term2 . pick #x3)
-                      . (feSub . feMul)
-                  )
-                . (roll #s1 . pick #h . term3)
-                . feSub
+                . (roll #u1 . pick #h . term2 . pick #x3 . feSub . roll #r)
+                . (feMul . roll #s1 . pick #h . term3 . feSub)
             )
           . name #z3 (roll #h . roll #z1 . feMul . roll #z2 . feMul)
           . (roll #x3 . roll #y3 . roll #z3 . makePoint . del #p1)
       )
+
+coeff :: Integer -> Fn (s > TInt) (s > TInt)
+coeff c = int c . feMul
 
 term2 :: Fn (s > TInt > TInt) (s > TInt)
 term2 = feSquare . feMul
