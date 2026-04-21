475eecf1aec5029a0e4d1247e729048d3cb77665
albaDsl <albadsl@proton.me>
2026-04-21 19:14:31 +0200

    Rename expressions. Update comments.

--- apps/demo/Demo.hs
+++ apps/demo/Demo.hs
@@ -3,16 +3,16 @@
 
 module Demo
   ( module DemoPrelude,
-    f1,
-    f2,
-    f3,
-    f4,
-    f5,
-    f6,
-    f7,
-    f8,
-    f9,
-    f10,
+    e1,
+    e2,
+    e3,
+    e4,
+    e5,
+    e6,
+    e7,
+    e8,
+    e9,
+    e10,
     prop1,
     prop2,
     prop3,
@@ -39,15 +39,15 @@ import DslDemo.TurtleVm.Bch2026.TurtleVm qualified as T26
 import Prelude hiding (foldl, (.))
 
 -- Example 1. Write code to multiply 3 by 7.
-f1 =
+e1 =
   begin
     . int 3
     . int 7
     . opMul
 
 -- Example 2. Implement a function that calculates x^2 - 2*x
-f2 :: S (s :> TInt) alt -> S (s :> TInt) alt
-f2 =
+e2 :: S (s :> TInt) alt -> S (s :> TInt) alt
+e2 =
   begin
     . opDup
     . square
@@ -60,14 +60,14 @@ f2 =
     coeff c = int c . opMul
 
 prop1 :: Integer -> Property
-prop1 x = ev (c f2) x === x ^ 2 - 2 * x
+prop1 x = ev (c e2) x === x ^ 2 - 2 * x
 
 prop2 :: Integer -> Property
-prop2 x = (ev (c f2) x >= 0) === True
+prop2 x = (ev (c e2) x >= 0) === True
 
 -- Example 3. Implement a function that calculates x^3 - x^2 + 2*x
-f3 :: S (s :> TInt) alt -> S (s :> TInt) alt
-f3 =
+e3 :: S (s :> TInt) alt -> S (s :> TInt) alt
+e3 =
   begin
     . ns #x
     . pick #x
@@ -85,45 +85,45 @@ f3 =
     coeff c = int c . opMul
 
 prop3 :: Integer -> Property
-prop3 x = ev (c f3) x === x ^ 3 - x ^ 2 + 2 * x
+prop3 x = ev (c e3) x === x ^ 3 - x ^ 2 + 2 * x
 
 -- Example 4. Demo of the recursive pow function.
-f4 :: S (s :> TNat) alt -> S (s :> TInt) alt
-f4 = int 2 . opSwap . Exp.pow
+e4 :: S (s :> TNat) alt -> S (s :> TInt) alt
+e4 = int 2 . opSwap . Exp.pow
 
 -- Example 5. Demo of the loops based pow function.
-f5 :: S (s :> TNat) alt -> S (s :> TInt) alt
-f5 = int 2 . opSwap . pow
+e5 :: S (s :> TNat) alt -> S (s :> TInt) alt
+e5 = int 2 . opSwap . pow
 
 -- Example 6. Secp256k1 point multiplication. Calculates n * G and returns the
 -- x-coordinate. Try with e.g. test vectors from:
 -- https://crypto.stackexchange.com/questions/784/
 -- are-there-any-secp256k1-ecdsa-test-examples-available
--- ev (c f6) 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140
-f6 :: S (s :> TNat) alt -> S (s :> TInt) alt
-f6 = EC.g . EC.ecMul . EC.getX
+-- ev (c e6) 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140
+e6 :: S (s :> TNat) alt -> S (s :> TInt) alt
+e6 = EC.g . EC.ecMul . EC.getX
 
--- Example 7. Evaluating =f3= using turtleVm (Bch2025).
--- evl (c f7)
-f7 :: Fn (s :> TInt) (s :> TInt)
-f7 = bytes (c f3) . toTyped (T25.turtleVm 20 5)
+-- Example 7. Evaluating =e3= using turtleVm (Bch2025).
+-- evl (c e7) 3
+e7 :: Fn (s :> TInt) (s :> TInt)
+e7 = bytes (c e3) . toTyped (T25.turtleVm 20 5)
 
 prop4 :: Integer -> Property
-prop4 x = ev (c f7) x === x ^ 3 - x ^ 2 + 2 * x
+prop4 x = ev (c e7) x === x ^ 3 - x ^ 2 + 2 * x
 
--- Example 8. Evaluating =f3= using turtleVm (Bch2026).
--- evl (c f8)
-f8 :: Fn (s :> TInt) (s :> TInt)
-f8 = bytes (c f3) . toTyped (T26.turtleVm 5)
+-- Example 8. Evaluating =e3= using turtleVm (Bch2026).
+-- evl (c e8) 3
+e8 :: Fn (s :> TInt) (s :> TInt)
+e8 = bytes (c e3) . toTyped (T26.turtleVm 5)
 
 prop5 :: Integer -> Property
-prop5 x = ev (c f8) x === x ^ 3 - x ^ 2 + 2 * x
+prop5 x = ev (c e8) x === x ^ 3 - x ^ 2 + 2 * x
 
 -- Example 9. Evaluate one solution to the miniTurtleVm101 challenge on
 -- miniTurtleVm101 running on top of turtleVm Bch2026.
--- ev (c f9) 0
-f9 :: Fn (s :> TInt) (s :> TInt)
-f9 =
+-- ev (c e9) 0
+e9 :: Fn (s :> TInt) (s :> TInt)
+e9 =
   begin
     . bytes solution
     . bytes (c (toTyped miniTurtleVm101))
@@ -132,12 +132,14 @@ f9 =
     solution :: Bytes
     solution = [0x02, 0x8b, 0x95, 0x89, 0x51, 0x8b, 0x51, 0x8a, 0x8b]
 
+-- Example 10. Usage of the Vector library.
+-- ev (c e10) 0
 -- >>> import Alba.Dsl.V1.Bch2026 qualified as Dsl
 -- >>> import Alba.Dsl.V1.Bch2026.Contract.Integral (Integral (..))
--- >>> Dsl.progSize f10
--- "22 opcodes, 25 bytes. Including function table: 88 opcodes, 397 bytes.\n"
-f10 :: Fn (s :> TInt) (s :> TInt)
-f10 = runEnv (opDrop . f)
+-- >>> Dsl.progSize e10
+-- "22 opcodes, 25 bytes. Total (with function table): 88 opcodes, 418 bytes.\n"
+e10 :: Fn (s :> TInt) (s :> TInt)
+e10 = runEnv (opDrop . f)
   where
     f :: Env s (s :> TInt)
     f =
