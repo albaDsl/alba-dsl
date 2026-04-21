9ae492f00d94fc96341f753914bf617f203de504
albaDsl <albadsl@proton.me>
2026-04-21 19:14:31 +0200

    Refactor math functions. Drop pow''.

--- src/Alba/Dsl/V1/Bch2026/Contract/Math.hs
+++ src/Alba/Dsl/V1/Bch2026/Contract/Math.hs
@@ -1,131 +1,68 @@
 module Alba.Dsl.V1.Bch2026.Contract.Math
   ( pow,
     pow',
-    pow'',
     factorial,
   )
 where
 
-import Alba.Dsl.V1.Bch2025.Contract.Prelude
-  ( halve,
-    ifZero,
-    isOdd,
-    isZero,
-    nat1SubUnsafe,
-  )
-import Alba.Dsl.V1.Bch2026
+import Alba.Dsl.V1.Bch2025
   ( Fn,
-    N,
-    Stack (..),
-    StackEntry,
-    TBool,
+    Stack ((:>)),
     TInt,
     TNat,
-    ex1,
     int,
+    name,
     nat,
-    op1,
+    ns2,
+    ns3,
     op2Drop,
-    opDrop,
-    opDup,
-    opUntil,
     opWhen,
     pick,
     roll,
-    unname,
+    un,
   )
+import Alba.Dsl.V1.Bch2025.Contract.Math (halve, isOdd)
+import Alba.Dsl.V1.Bch2025.Contract.Misc
+  ( ifZero,
+    isZero,
+    nat1SubUnsafe,
+  )
+import Alba.Dsl.V1.Bch2026 (Loop, fn, opUntil)
 import Alba.Dsl.V1.Bch2026.Contract.Integral (Integral (..))
-import Alba.Dsl.V1.Bch2026.Contract.Shorthand (nip, over, rot, swap)
+import Alba.Dsl.V1.Bch2026.Contract.Shorthand (drop, dup, nip, swap)
 import Alba.Dsl.V1.Common.Lang (begin, (.))
 import Prelude ()
 
--- 35 opcodes, 35 bytes.
+-- >>> import Alba.Dsl.V1.Bch2026
+-- >>> progSize pow
+-- "2 opcodes, 2 bytes. Total (with function table): 5 opcodes, 39 bytes.\n"
 pow :: Fn (s :> TInt :> TNat) (s :> TInt)
-pow = pow' mul
+pow = fn (pow' mul)
 
--- The multiplication operator to use is provided as an argument.
+-- Macro for calculating pow. The multiplication operator to use is provided as
+-- an argument.
 pow' ::
   (forall s'. Fn (s' :> TInt :> TInt) (s' :> TInt)) ->
   Fn (s :> TInt :> TNat) (s :> TInt)
-pow' f =
-  begin
-    . opDup
-    . ifZero
-      (op2Drop . int 1)
-      (int 1 . opUntil (unname 3 fn) . nip . nip)
+pow' f = dup . ifZero (op2Drop . int 1) (int 1 . opUntil loop . nip . nip)
   where
-    fn ::
-      Fn
-        (s :> N "b" TInt :> N "n" TNat :> N "res" TInt)
-        (s :> TInt :> TNat :> TInt :> TBool)
-    fn =
+    loop :: Loop (s :> TInt :> TNat :> TInt)
+    loop =
       begin
-        . roll #res -- <args> res
-        . ex1 (pick #n . isOdd) -- <args> res odd?
-        . opWhen (pick #b . f) -- <args> res'
-        . (roll #b . square') -- <args> res' b
-        . swap -- <args> b res'
-        . (roll #n . halve) -- <args> b res' n
-        . ex1 (opDup . isZero) -- b res' n zero?
-        . rot -- b n zero? res'
-        . swap -- b n res' zero?
-    square' :: Fn (s :> TInt) (s :> TInt)
-    square' = opDup . f
+        . ns3 #b #n #res
+        . (roll #res . pick #n . isOdd . opWhen (pick #b . f))
+        . (roll #b . square' . swap)
+        . (name #n' (roll #n . halve) . swap)
+        . (pick #n' . isZero . un #n')
 
--- The multiplication operator to use is provided as an argument. The
--- operator also expects some arbitrary data that gets passed in as an
--- argument to pow.
-pow'' ::
-  forall s t.
-  (StackEntry t) =>
-  (forall s'. Fn (s' :> TInt :> TInt :> t) (s' :> TInt)) ->
-  Fn (s :> TInt :> TNat :> t) (s :> TInt)
-pow'' f =
-  begin
-    . swap
-    . opDup
-    . ifZero
-      (op2Drop . opDrop . int 1)
-      ( begin
-          . swap
-          . int 1
-          . swap
-          . opUntil (unname 4 fn)
-          . opDrop
-          . nip
-          . nip
-      )
-  where
-    fn ::
-      Fn
-        (s :> N "b" TInt :> N "n" TNat :> N "res" TInt :> N "data" t)
-        (s :> TInt :> TNat :> TInt :> t :> TBool)
-    fn =
-      begin
-        . roll #res -- <args> res
-        . ex1 (pick #n . isOdd) -- <args> res odd?
-        . opWhen (pick #b . pick #data . f) -- <args> res'
-        . (roll #b . pick #data . square') -- <args> res' b'
-        . swap -- <args> b' res'
-        . (roll #n . halve) -- <args> b' res' n'
-        . ex1 (opDup . isZero) -- <args> b' res' n' zero?
-        . rot -- <args> b' n' zero? res'
-        . roll #data -- b' n' zero? res' data
-        . rot -- <args> b' n' res' data zero?
-    square' :: forall s'. Fn (s' :> TInt :> t) (s' :> TInt)
-    square' = over . swap . f
+    square' :: Fn (s :> TInt) (s :> TInt)
+    square' = dup . f
 
 factorial :: Fn (s :> TNat) (s :> TNat)
-factorial =
-  begin
-    . opDup
-    . ifZero
-      (opDrop . op1)
-      (nat 1 . swap . opUntil (unname 2 fn) . opDrop)
+factorial = dup . ifZero (drop . nat 1) (nat 1 . swap . opUntil loop . drop)
   where
-    fn :: Fn (s :> N "product" TNat :> N "n" TNat) (s :> TNat :> TNat :> TBool)
-    fn =
+    loop :: Loop (s :> TNat :> TNat)
+    loop =
       begin
-        . (roll #product . pick #n . mul)
-        . (roll #n . nat1SubUnsafe)
-        . (opDup . isZero)
+        . (ns2 #product #n . roll #product . pick #n . mul)
+        . (roll #n . nat1SubUnsafe . dup . isZero)
