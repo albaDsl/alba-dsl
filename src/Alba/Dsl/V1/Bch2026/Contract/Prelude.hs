4acc258f6dab32085ace27ad70f30976afd8e32e
albaDsl <albadsl@proton.me>
2026-06-24 08:54:23 +0200

    Add TInt264.

--- src/Alba/Dsl/V1/Bch2026/Contract/Prelude.hs
+++ src/Alba/Dsl/V1/Bch2026/Contract/Prelude.hs
@@ -19,6 +19,7 @@ module Alba.Dsl.V1.Bch2026.Contract.Prelude
     module Alba.Dsl.V1.Bch2026.Contract.TBytes128,
     module Alba.Dsl.V1.Bch2026.Contract.TEither,
     module Alba.Dsl.V1.Bch2026.Contract.TInt16,
+    module Alba.Dsl.V1.Bch2026.Contract.TInt264,
     module Alba.Dsl.V1.Bch2026.Contract.TInt64,
     module Alba.Dsl.V1.Bch2026.Contract.TInt8,
     module Alba.Dsl.V1.Bch2026.Contract.TMaybe,
@@ -46,6 +47,7 @@ import Alba.Dsl.V1.Bch2026.Contract.Shorthand
 import Alba.Dsl.V1.Bch2026.Contract.TBytes128
 import Alba.Dsl.V1.Bch2026.Contract.TEither
 import Alba.Dsl.V1.Bch2026.Contract.TInt16
+import Alba.Dsl.V1.Bch2026.Contract.TInt264
 import Alba.Dsl.V1.Bch2026.Contract.TInt64
 import Alba.Dsl.V1.Bch2026.Contract.TInt8
 import Alba.Dsl.V1.Bch2026.Contract.TMaybe
---
+++ src/Alba/Dsl/V1/Bch2026/Contract/TInt264.hs
@@ -0,0 +1,125 @@
+-- Copyright (c) 2026 albaDsl
+
+-- 264-bit (33 byte) integers. Allows for storage of e.g. 256-bit quantities
+-- which a 32 byte integer would not due to the sign bit.
+
+module Alba.Dsl.V1.Bch2026.Contract.TInt264 (TInt264, int264) where
+
+import Alba.Dsl.V1.Bch2026
+  ( Fn,
+    Stack (..),
+    StackEntry,
+    TInt,
+    begin,
+    cast,
+    castStack,
+    constant,
+    fn,
+    int,
+    lambda1,
+    lambda2,
+    nat,
+    op1Add,
+    op1Sub,
+    opAbs,
+    opAdd,
+    opBin2Num,
+    opDiv,
+    opGreaterThan,
+    opGreaterThanOrEqual,
+    opLessThan,
+    opLessThanOrEqual,
+    opMax,
+    opMin,
+    opMod,
+    opMul,
+    opNegate,
+    opNum2Bin,
+    opSub,
+    opVerify,
+    opWithin,
+    (.),
+  )
+import Alba.Dsl.V1.Bch2026.Contract.BlobEqClass (BlobEq (..))
+import Alba.Dsl.V1.Bch2026.Contract.BlobEqUtils
+  ( blobEqEqual,
+    blobEqEqualVerify,
+    blobEqRecord,
+  )
+import Alba.Dsl.V1.Bch2026.Contract.Integral (Integral (..))
+import Alba.Dsl.V1.Bch2026.Contract.Ord (Ord (..), mkOrdM)
+import Alba.Dsl.V1.Bch2026.Contract.PackFs (PackFs (..), TPackFs, mkPackFsM)
+import Alba.Dsl.V1.Bch2026.Contract.Shorthand (dup)
+import Control.Exception (assert)
+import Prelude (Integer, (&&), (-), (<=), (>=), (^))
+
+data TInt264
+
+instance StackEntry TInt264
+
+instance BlobEq TInt264 where
+  equal = blobEqEqual
+  equalVerify = blobEqEqualVerify
+  blobEqRec = blobEqRecord
+
+instance Ord TInt264 where
+  lessThan = toRaw2 . opLessThan
+  lessThanOrEqual = toRaw2 . opLessThanOrEqual
+  greaterThan = toRaw2 . opGreaterThan
+  greaterThanOrEqual = toRaw2 . opGreaterThanOrEqual
+  min = toRaw2 . opMin . fromRaw
+  max = toRaw2 . opMax . fromRaw
+  within = toRaw3 . opWithin
+  ordRec = lambda2 (lessThanOrEqual @TInt264) . mkOrdM
+
+instance Integral TInt264 where
+  add = toRaw2 . opAdd . fromInt
+  add1 = toRaw . op1Add . fromInt
+  sub = toRaw2 . opSub . fromInt
+  sub1 = toRaw . op1Sub . fromInt
+  mul = toRaw2 . opMul . fromInt
+  div = toRaw2 . opDiv . fromInt
+  mod = toRaw2 . opMod . fromInt
+  negate = toRaw . opNegate . fromRaw
+  abs = toRaw . opAbs . fromRaw
+  fromInt =
+    fn (dup . int int264Min . int int264Max . opWithin . opVerify . fromRaw)
+  toInt = toRaw
+
+instance PackFs TInt264 where
+  sizeConst = 33
+  size = nat (sizeConst @TInt264)
+  pack = toRaw . size @TInt264 . opNum2Bin
+  unpack = opBin2Num . fromRaw
+  packFsRec = int264PackFs
+
+int264PackFs :: Fn s (s :> TPackFs TInt264)
+int264PackFs =
+  constant
+    ( begin
+        . size @TInt264
+        . lambda1 (pack @TInt264)
+        . lambda1 (unpack @TInt264)
+        . mkPackFsM
+    )
+
+int264Max :: Integer
+int264Max = 2 ^ (263 :: Integer) - 1
+
+int264Min :: Integer
+int264Min = -int264Max
+
+int264 :: Integer -> Fn s (s :> TInt264)
+int264 x = assert (x >= int264Min && x <= int264Max) (int x . fromRaw)
+
+fromRaw :: Fn (s :> TInt) (s :> TInt264)
+fromRaw = cast
+
+toRaw :: Fn (s :> TInt264) (s :> TInt)
+toRaw = cast
+
+toRaw2 :: Fn (s :> TInt264 :> TInt264) (s :> TInt :> TInt)
+toRaw2 = castStack
+
+toRaw3 :: Fn (s :> TInt264 :> TInt264 :> TInt264) (s :> TInt :> TInt :> TInt)
+toRaw3 = castStack
