fc8929eed5a7ce7998e7545dd519ba4b528cbc1e
albaDsl <albadsl@proton.me>
2026-06-24 08:54:23 +0200

    Optimize eval.

--- src/Alba/Dsl/V1/Common/RuntimeLibUntyped.hs
+++ src/Alba/Dsl/V1/Common/RuntimeLibUntyped.hs
@@ -1,8 +1,8 @@
 -- Copyright (c) 2026 albaDsl
 
-module Alba.Dsl.V1.Common.RuntimeLibUntyped (eval, invoke) where
+module Alba.Dsl.V1.Common.RuntimeLibUntyped (invoke) where
 
-import Alba.Dsl.V1.Bch2026.LangUntyped
+import Alba.Dsl.V1.Bch2026.LangUntyped (bytes, fn, int)
 import Alba.Dsl.V1.Bch2026.OpsUntyped
   ( opBin2Num,
     opCat,
@@ -20,10 +20,9 @@ import Alba.Dsl.V1.Bch2026.OpsUntyped
     opToAltStack,
     opTrue,
     opUntil,
-    opVerify,
   )
 import Alba.Dsl.V1.Common.OpcodeL3 (localIdMaxLength)
-import Alba.Dsl.V1.Common.StackUntyped (FnU, SU, (.))
+import Alba.Dsl.V1.Common.StackUntyped (FnU, (.))
 import Data.ByteString qualified as B
 import Prelude (fromIntegral)
 
@@ -40,8 +39,9 @@ invoke =
         . (opLessThanOrEqual . opIf opInvoke eval)
     )
 
--- Evaluator that only handles OP_PUSHDATA2 and OP_INVOKE. It does not enforce
--- the minimal push requirement. Code to evaluate expected at top of stack.
+-- Evaluator that ONLY handles OP_PUSHDATA2 and OP_INVOKE. It does not enforce
+-- the minimal push requirement. OP_INVOKEed code can use-and-restore but not
+-- modify the alt-stack. Code to evaluate expected at top of stack.
 eval :: FnU
 eval =
   fn
@@ -49,34 +49,17 @@ eval =
         ( (opDup . bytes [] . opEqual)
             . opIf
               (opDrop . opTrue)
-              ( (int 1 . opSplit . opSwap)
-                  . case'
-                    [ ( bytesEq pushdataOp,
-                        opDrop . int 2 . opSplit . opSwap . toSigned . opSplit
-                      ),
-                      ( bytesEq invokeOp,
-                        opDrop . opToAltStack . opInvoke . opFromAltStack
-                      )
-                    ]
-                    (bytes "E0" . opFalse . opVerify)
+              ( (int 1 . opSplit . opSwap . bytes pushdataOp . opEqual)
+                  . opIf
+                    (int 2 . opSplit . opSwap . toSigned . opSplit)
+                    (opToAltStack . opInvoke . opFromAltStack)
                   . opFalse
               )
         )
     )
   where
-    bytesEq :: B.ByteString -> FnU
-    bytesEq x = bytes x . opEqual
-
     pushdataOp :: B.ByteString
     pushdataOp = [0x4d]
 
     toSigned :: FnU
     toSigned = bytes [0] . opCat . opBin2Num
-
-    invokeOp :: B.ByteString
-    invokeOp = [0x8a]
-
-case' :: [(SU -> SU, SU -> SU)] -> (SU -> SU) -> (SU -> SU)
-case' [] def st = def st
-case' ((test, result) : rest) def st =
-  (opDup . test . opIf result (case' rest def)) st
